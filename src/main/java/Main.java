ef4f9b9a600cbae1fe6d2cd1933166affe6f288b
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2024-05-09 20:30:12 -0600

    Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import Time.IPAddress;
+import Utils.GetMyIP;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+        IPAddress test = new IPAddress();
+        GetMyIP iipp = new GetMyIP();
+        test.GetTimeByIPAddress(iipp.returnMyIP());
+    }
 }
--- src/main/java/Utils/GetMyIP.java
+++ src/main/java/Utils/GetMyIP.java
@@ -1,2 +1,44 @@
-package Utils;public class GetMyIP {
+package Utils;
+
+import java.net.*;
+import java.util.Enumeration;
+import java.util.regex.*;
+
+public class GetMyIP {
+public String returnMyIP() {
+    String myAddress = "0.0.0.0";
+    try {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface iface = interfaces.nextElement();
+            if (iface.isLoopback() || !iface.isUp()) continue;
+            Enumeration<InetAddress> addresses = iface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+
+                if (isValidIPAddress(addr.getHostAddress())) {
+                    System.out.println(iface.getDisplayName() + " " + addr.getHostAddress());
+                    myAddress = addr.getHostAddress();
+                }
+            }
+        }
+    } catch (SocketException e) {
+        throw new RuntimeException(e);
+    }
+
+    return myAddress;
+}
+
+public static boolean isValidIPAddress(String ip) {
+    String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    String regex = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+
+    Pattern p = Pattern.compile(regex);
+    if (ip == null) {
+        return false;
+    }
+
+    Matcher m = p.matcher(ip);
+    return m.matches();
+}
 }
